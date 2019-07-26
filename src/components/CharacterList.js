fa04f92347b74a987d9bb46d5103e0b7da691d1f
noahfranco <noahfra@gmail.com>
2019-07-26 18:56:09 -0400

    adding <Link> tags and finishing up

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,17 +1,32 @@
 import React, { useEffect, useState } from 'react';
+import axios from "axios"
+import CharacterCard from "./CharacterCard.js"
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 
-  
+const [characterList, setCharacterList] = useState([]); 
+
   useEffect(() => {
+    axios
+    .get("https://rickandmortyapi.com/api/character/")
+    .then((value) => {
+      console.log(value)
+      setCharacterList(value.data.results)
+    })
+    .catch(error => {
+      console.log("ERROR!", error);
+    });
+    
     // TODO: Add AJAX/API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
-
-  return <section className='character-list grid-view'>
-
-      <h2>TODO: `array.map()` over your state here!</h2>
+  }, []); 
+  return(
+    <section className="character-list grid-view"> 
+    { characterList.map(character => {
+      return <CharacterCard key={character.id} character={ character } /> 
+    })}
     </section>
-
+  )
 }
--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,5 +1,29 @@
 import React, { useEffect, useState } from 'react';
+import axios from "axios"; 
+import LocationCard from "./LocationCard.js"; 
 
 export default function LocationsList() {
+
+const [locationList, setLocationList] = useState([])
+
+useEffect(() => {
+    axios
+    .get("https://rickandmortyapi.com/api/location/")
+    .then(here => {
+     console.log(here)
+     setLocationList(here.data.results); 
+    })
+    .catch(error => {
+        console.log("ERROR!", error);
+      });
+}, [])
+
+return (
+    <section className="main grid-view"> 
+    { locationList.map(location => {
+      return <LocationCard key={location.id} location={ location } /> 
+    })}
+    </section>
+  )
 
 }
